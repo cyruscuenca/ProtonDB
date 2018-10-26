eaa3272db43d29be22d198f513b6f8140ff05221
Cyrus Cuenca <cyrushcuenca@gmail.com>
2018-10-26 14:12:26 -0700

    Updating create page again

--- resources/views/frontend/entry/create.blade.php
+++ resources/views/frontend/entry/create.blade.php
@@ -16,7 +16,7 @@
 <div style="width: 100%; background: #2a475e; margin-top: 50px">
     <div style="background: #30516C; height: 50px; width: 100%;">
         <div style="margin-left: 25px; margin-right: 25px; width: calc(100% - 50px);">
-            <h6>Create a Test Report</h6>
+            <h6 style="margin-top: 10px;">Create a Test Report</h6>
         </div>
     </div>
     <div style="min-height: 400px; color: #C7D5E0;">
@@ -29,7 +29,7 @@
                 {{ Form::label("Distro&nbsp;", null) }}
                 {!! Form::select("distro_id", $distros, null, ['placeholder' => '&nbsp;&nbsp;Specify your distro&nbsp;&nbsp;', 'style' => 'display: inline-block; color: #C7D5E0; border: none; height: 30px; border-radius: 1px;']) !!}
                 </div>
-                {!! Form::text("distro_version", null, ['placeholder' => 'Version', 'class' => 'form-control', 'style' => 'width: 115px; display: inline-block; color: #C7D5E0; height: 35px; background: #C7D5E0; border: 1px solid #2A475E;']) !!}
+                {!! Form::text("distro_version", null, ['placeholder' => 'Version', 'class' => 'form-control', 'style' => 'width: 115px; display: inline-block; border-radius: 1px; color: #C7D5E0; height: 50px; background: #30516C;']) !!}
             </div>
             <div style="width: 100%; margin-bottom: 18px;">
                 <div style="margin-bottom: 18px;">
@@ -42,7 +42,7 @@
                 {!! Form::select("gpu_id", $gpus, null, ['placeholder' => '&nbsp;&nbsp;Specify your GPU&nbsp;&nbsp;', 'style' => 'color: #C7D5E0; border: none; height: 30px; border-radius: 1px;']) !!}
                 </div>
                 <div style="display: inline-block; margin-right: 15px;">
-                {!! Form::text("driver_version", null, ['placeholder' => 'Driver', 'class' => 'form-control', 'style' => 'width: 115px; display: inline-block; color: #C7D5E0; height: 35px; background: #C7D5E0; border: 1px solid #2A475E;']) !!}
+                {!! Form::text("driver_version", null, ['placeholder' => 'Driver', 'class' => 'form-control', 'style' => 'width: 115px; display: inline-block; border-radius: 1px; color: #C7D5E0; height: 50px; background: #30516C;']) !!}
                 </div>
                 </div>
             </div>
@@ -51,11 +51,11 @@
         <div style="margin: 25px; color: #C7D5E0;">
             <div style="width: calc(50% - 12.5px); display: inline-block; float: left;">
             {{ Form::label("What works?", null) }}
-            {!! Form::textarea("works", null, ['class' => 'form-control', 'style' => 'border-radius: 1px; color: #C7D5E0; height: 50px; background: #30516C; border: 1px solid #1b2838;']) !!}
+            {!! Form::textarea("works", null, ['class' => 'form-control', 'style' => 'border-radius: 1px; color: #C7D5E0; height: 50px; background: #30516C;']) !!}
             </div>
             <div style="width: calc(50% - 12.5px); margin-bottom: 25px; display: inline-block; float: right;">
             {{ Form::label("What's broken?", null) }}
-            {!! Form::textarea("broken", null, ['class' => 'form-control', 'style' => 'border-radius: 1px; color: #C7D5E0; height: 50px; background: #30516C; border: 1px solid #1b2838;']) !!}
+            {!! Form::textarea("broken", null, ['class' => 'form-control', 'style' => 'border-radius: 1px; color: #C7D5E0; height: 50px; background: #30516C;']) !!}
             </div>
 			<div style="width: 100%;">
             	<p style="display: inline-block;">Did you try any tweaks to get the game running better?&nbsp;</p>
@@ -64,20 +64,20 @@
 			<div id="autoUpdate" style="height: 210px;">
 				<div style="width: 100%; margin-bottom: 25px;">
 				{{ Form::label("What tweaks did you do?", null) }}
-				{!! Form::textarea("tweaks", null, ['class' => 'form-control textbox', 'style' => 'border-radius: 1px; color: #C7D5E0; height: 50px; background: #30516C; border: 1px solid #1b2838;']) !!}
+				{!! Form::textarea("tweaks", null, ['class' => 'form-control textbox', 'style' => 'border-radius: 1px; color: #C7D5E0; height: 50px; background: #30516C;']) !!}
 				</div>
                 <div style="width: calc(50% - 12.5px); display: inline-block; float: left;">
                 {{ Form::label("What works now?", null) }}
-                {!! Form::textarea("works_after", null, ['class' => 'form-control', 'style' => 'border-radius: 1px; color: #C7D5E0; height: 50px; background: #30516C; border: 1px solid #1b2838;']) !!}
+                {!! Form::textarea("works_after", null, ['class' => 'form-control', 'style' => 'border-radius: 1px; color: #C7D5E0; height: 50px; background: #30516C;]) !!}
                 </div>
                 <div style="width: calc(50% - 12.5px); display: inline-block; float: right;">
                 {{ Form::label("What's broken now?", null) }}
-                {!! Form::textarea("broken_after", null, ['class' => 'form-control', 'style' => 'border-radius: 1px; color: #C7D5E0; height: 50px; background: #30516C; border: 1px solid #1b2838;']) !!}
+                {!! Form::textarea("broken_after", null, ['class' => 'form-control', 'style' => 'border-radius: 1px; color: #C7D5E0; height: 50px; background: #30516C;']) !!}
                 </div>
 			</div>
 			<div style="width: 100%; margin-bottom: 25px;">
 				{{ Form::label("Extra notes", null) }}
-				{!! Form::textarea("notes", null, ['class' => 'form-control', 'style' => 'border-radius: 1px; color: #2a475e; height: 50px; background: #C7D5E0; border: 1px solid #1b2838;']) !!}
+				{!! Form::textarea("notes", null, ['class' => 'form-control', 'style' => 'border-radius: 1px; color: #C7D5E0; height: 50px; background: #30516C;']) !!}
 			</div>
         </div>
         <div style="color: #C7D5E0;">
